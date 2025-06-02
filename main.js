29a53151ebdb70f2551b3210edd2ee70d35784ba
JoaoL <joaolucaslucasnascimento@gmail.com>
2025-06-02 12:31:31 -0300

    Página de Blog

--- main.js
+++ main.js
@@ -1,24 +1,3 @@
-
-  //carrosel
-  var radio = document.querySelector('.manual-btn') 
-  var cont = 1
-  
-  document.getElementById('radio1').checked = true
-  
-  setInterval(() => {
-      proximaImg()
-  }, 5000)
-  
-  function proximaImg(){
-    cont++
-  
-    if(cont > 3){
-      cont = 1
-    }
-  
-    document.getElementById('radio' + cont).checked = true
-  } 
-
 //BARRA MENU
 
 function abrirMenu() {
@@ -42,41 +21,3 @@ window.addEventListener("scroll", function(){
       scrollTopButton.style.display = "none";
   }
 };
-
-
-//TESTE CARROSSEL 02
-let currentSlide = 0;
-
-function showSlide(index) {
-    const slides = document.querySelector('.carrossel');
-    const totalSlides = document.querySelectorAll('.slide').length;
-
-    // Restringe o índice
-    if (index >= totalSlides) {
-        currentSlide = 0;
-    } else if (index < 0) {
-        currentSlide = totalSlides - 1;
-    } else {
-        currentSlide = index;
-    }
-
-    // Move os slides
-    slides.style.transform = 'translateX(' + (-currentSlide * 100) + '%)';
-}
-
-// Funções para navegação
-function nextSlide() {
-    showSlide(currentSlide + 1);
-}
-
-function prevSlide() {
-    showSlide(currentSlide - 1);
-}
-
-// Adiciona os event listeners para os botões de navegação
-document.querySelector('.prev').addEventListener('click', prevSlide);
-document.querySelector('.next').addEventListener('click', nextSlide);
-
-// Inicia mostrando o primeiro slide
-showSlide(currentSlide);
-
